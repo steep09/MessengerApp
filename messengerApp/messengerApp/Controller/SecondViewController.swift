1bb8f7f4c91cf589da7ab7d6f913c1613360766a
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-06-26 21:49:57 +0800

    initial commit

---
+++ messengerApp/messengerApp/Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  messengerApp
+//
+//  Created by Stephenson Ang on 26/06/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

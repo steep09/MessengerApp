8d41a61baaf00f45656977660e83be146ce1128f
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-06-26 22:29:45 +0800

    setup databases for users, groups, and feed

---
+++ messengerApp/messengerApp/Services/DataService.swift
@@ -0,0 +1,42 @@
+//
+//  DataService.swift
+//  messengerApp
+//
+//  Created by Stephenson Ang on 26/06/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    //it's like creating a folder for all the users
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_USERS: DatabaseReference {
+        return _REF_USERS
+    }
+    
+    var REF_GROUPS: DatabaseReference {
+        return _REF_GROUPS
+    }
+    
+    var REF_FEED: DatabaseReference {
+        return _REF_FEED
+    }
+    
+    func createDBUser(uid: String, userData: Dictionary<String, Any>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+}

8e690b4d6f5031eabfdebf1f95843bcec3b43238
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-07-02 12:04:24 +0800

    setup AuthService file and add login and register user function

---
+++ messengerApp/messengerApp/Services/AuthService.swift
@@ -0,0 +1,42 @@
+//
+//  AuthService.swift
+//  messengerApp
+//
+//  Created by Stephenson Ang on 02/07/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.user.providerID, "email": user.user.email]
+            DataService.instance.createDBUser(uid: user.user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+        
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                loginComplete(false, error)
+                return
+            }
+            
+            loginComplete(true, nil)
+        }
+        
+    }
+}
